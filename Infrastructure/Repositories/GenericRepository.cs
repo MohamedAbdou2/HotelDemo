b21eb7f76f48650241e1596ebbe3b651907c5980
omarsayed101 <omarsayed.mail@gmail.com>
2026-01-23 17:27:34 +0200

    ------

---
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Domain.Repositories;
+
+namespace Infrastructure.Repositories
+{
+    public class GenericRepository<T>:IGenericRepository<T> where T : BaseModel
+    {
+    }
+}
