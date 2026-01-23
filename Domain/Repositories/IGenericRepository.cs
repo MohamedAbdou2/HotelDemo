b1e83c48a3579af227af3d3efede40df91270137
omarsayed101 <omarsayed.mail@gmail.com>
2026-01-23 18:53:00 +0200

    Adding Generic Repository

--- Domain/Repositories/IGenericRepository.cs
+++ Domain/Repositories/IGenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using Domain.Models;
@@ -9,6 +10,17 @@ namespace Domain.Repositories
 {
     public  interface IGenericRepository<T> where T : BaseModel
     {
-        
+        Task<bool> Add(T entity);
+
+        Task<IQueryable<T>> GetAll(Expression<Func<T, bool>>? creiteria = null);
+
+        Task<IQueryable<T>> GetbyId(Guid Id);
+
+        Task<bool> UpdateIncludeAsync(T entity, params string[] modifiedParams);
+
+        Task<bool> IsExist(Guid Id);
+
+        Task<bool> Delete(Guid Id);
+
     }
 }
