400cce747c3665dcfc9abf10fed48b358689e65b
omarsayed101 <omarsayed.mail@gmail.com>
2026-01-25 22:14:35 +0200

    Adding User Service

---
+++ Application/Dtos/User/RegisterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.User
+{
+    public class RegisterDto
+    {
+        public string userName { get; set; }
+
+        public int phoneNumber { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string confirmPassword { get; set; }
+    }
+}
--- Domain/Enums/ErrorCode.cs
+++ Domain/Enums/ErrorCode.cs
@@ -2,6 +2,10 @@
 {
     public enum ErrorCode
     {
-
+        //User 
+        EmailalreadyExist = 101,
+        EmailNotRegistered = 102,
+        UserNotFound = 103,
+        InvalidOtp = 104
     }
 }
