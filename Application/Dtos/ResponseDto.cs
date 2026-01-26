d996ec040d71f9ba8037cfc221530af32a482b7d
mosaleh55 <www.mosaleh@gmail.com>
2026-01-26 12:53:53 +0300

    edit responseDto

--- Application/Dtos/ResponseDto.cs
+++ Application/Dtos/ResponseDto.cs
@@ -6,6 +6,8 @@ namespace Application.Dtos
 {
     public class ResponseDto<T>
     {
+     
+
         public T? Data { get; set; }
 
         public bool IsSuccess { get; set; }
@@ -13,28 +15,21 @@ namespace Application.Dtos
         public string Message { get; set; }
 
         public ErrorCode? ErrorCode { get; set; }
-
-        public static ResponseDto<T> Success(T data, string message = null)
+        protected ResponseDto(T? data, bool isSuccess, string message, ErrorCode? errorCode)
         {
-            return new ResponseDto<T>
-            {
-                Data = data,
-                IsSuccess = true,
-                Message = message,
-                ErrorCode = null
-            };
-
+            Data = data;
+            IsSuccess = isSuccess;
+            Message = message;
+            ErrorCode = errorCode;
+        }
+        public static ResponseDto<T> Success(T data, string? message = null)
+        {
+            return new ResponseDto<T>(data, true, message ?? "Success", null);
         }
 
-        public static ResponseDto<T>  Fail(ErrorCode? errorcode, string message)
+        public static ResponseDto<T>  Fail(ErrorCode? errorCode, string message)
         {
-            return new ResponseDto<T>
-            {
-                Data = default(T),
-                IsSuccess = false,
-                Message = message,
-                ErrorCode = errorcode
-            };
+            return new ResponseDto<T>(default, false, message, errorCode);
         }
         public static ResponseDto<T> ValidaitonFial(ValidationResult validationResult)
         {
