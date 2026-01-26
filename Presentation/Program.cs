57767128add18f0b67623c64526c9ff286a56d4e
mosaleh55 <www.mosaleh@gmail.com>
2026-01-26 12:53:53 +0300

    edit responseDto

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -11,28 +11,7 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<UnifiedValidationFilter>();
-}).AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-});
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
-
-builder.Services.AddApplicationServices();
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddHttpContextAccessor();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
