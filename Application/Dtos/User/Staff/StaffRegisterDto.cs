e9b0cc45e8d02719faa7090c2ea58fd0d5e9aea4
mosaleh55 <www.mosaleh@gmail.com>
2026-01-28 09:59:40 +0300

    Adding Staff , update Role

---
+++ Application/Dtos/User/Staff/StaffRegisterDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Application.Dtos.User.Staff
+{
+    public class StaffRegisterDto
+    {
+        public string userName { get; set; }
+
+        public int phoneNumber { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string confirmPassword { get; set; }
+        public string Position { get; set; } = null!;
+        public DateTime HireDate { get; set; } = DateTime.UtcNow;
+        public DateTime? TerminationDate { get; set; }
+
+    }
+}
---
+++ Application/Validator/UserValidator/updateValidatorRole.cs
@@ -0,0 +1,19 @@
+﻿using Application.Dtos.User;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validator.UserValidator
+{
+    public class updateValidatorRole : AbstractValidator<UpdateRoleDto>
+    {
+        public updateValidatorRole() 
+        { 
+         RuleFor(x => x.userId).NotEmpty().WithMessage("UserId is required");
+         RuleFor(x => x.roleName).NotEmpty().WithMessage("RoleName is required");
+        }
+    }
+}
