a1db95ba4ab5001773cdadf373785645b8c6b287
mosaleh5 <www.mosaleh@gmail.com>
2026-02-01 01:44:37 +0300

    Staff register end point

--- Presentation/Controllers/AdminController.cs
+++ Presentation/Controllers/AdminController.cs
@@ -1,8 +1,13 @@
-﻿using Application.Services;
+﻿using Application.Dtos.User.Staff;
+using Application.Helper;
+using Application.Services;
 using AutoMapper;
+using Domain.Enums;
+using Domain.Models;
 using HotelDemo.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.ViewModels.User.Staff;
 
 namespace Presentation.Controllers
 {
@@ -13,23 +18,34 @@ namespace Presentation.Controllers
     {
         private readonly UserService _userService;
         private readonly IMapper _mapper;
+        private readonly CurrentUser _currentUser;
 
-        public AdminController(UserService userService, IMapper mapper)
+        public AdminController(UserService userService,
+            IMapper mapper,
+            CurrentUser currentUser)
         {
             _userService = userService;
             _mapper = mapper;
+            this._currentUser = currentUser;
         }
-        /*       public ActionResult<ResultViewModel> GetAllUsers()
-               {
-                   var result = _userService.GetAllUsers();
-                   return Ok(result);
-               }*/
-
-
-        /* public ActionResult<ResponseViewModel<bool>> RegisterStaff()
+        /* public ActionResult<ResultViewModel> GetAllUsers()
          {
-             var result = _userService.StaffRegister();
+             var result = _userService.GetAllUsers();
              return Ok(result);
-         }*/
+         }
+ */
+       // [Authorize/*(Roles ="admin")*/]
+        [HttpPost]
+        public async Task<ActionResult<ResponseViewModel<bool>>> RegisterStaff([FromBody]StaffRegisterViewModel model)
+        {
+            var userId = _currentUser.GetUserId();
+            if (userId == Guid.Empty)
+                return ResponseViewModel<bool>.Fail(ErrorCode.UserNotFound, "Can not Find User");
+            var dto = _mapper.Map<StaffRegisterDto>(model);
+            var result = await _userService.StaffRegister(dto);
+            return result.Data ? 
+                ResponseViewModel<bool>.Success(result.Data , "staff register successfull")  
+                : ResponseViewModel<bool>.Fail(ErrorCode.StaffRegisterFail , "Error Aqure When regiter staff");
+        }
     }
 }
---
+++ Presentation/ViewModels/User/Staff/StaffRegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Presentation.ViewModels.User.Staff
+{
+    public class StaffRegisterViewModel
+    {
+        public string userName { get; set; }
+
+        public int phoneNumber { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string confirmPassword { get; set; }
+        public string Position { get; set; } = null!;
+        public DateTime HireDate { get; set; } = DateTime.UtcNow;
+        public DateTime? TerminationDate { get; set; }
+    }
+}
+
+
+
+
