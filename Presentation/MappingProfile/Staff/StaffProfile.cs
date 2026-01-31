70896859a82c6d7bbef4deebd0531ed9afd4b7a5
mosaleh5 <www.mosaleh@gmail.com>
2026-02-01 01:44:37 +0300

    Staff register end point

---
+++ Presentation/MappingProfile/Staff/StaffProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Presentation.ViewModels.User.Staff;
+
+namespace Presentation.MappingProfile.Staff
+{
+    public class StaffProfile : Profile
+    {
+        public StaffProfile()
+        {
+
+            CreateMap<StaffRegisterViewModel, StaffRegisterViewModel>();
+
+
+
+        }
+    }
+}
